8af065e0f34f21d3fc99987a57f1a2d0747782f4
Alexey Shishkin <shayu78@mail.ru>
2020-09-10 00:56:40 +0400

    Конфигурация с Eslint

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+const characters = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'маг', health: 0 },
+  { name: 'лучник', health: 0 },
+];
+
+const alive = characters.filter((item) => item.health > 0);
